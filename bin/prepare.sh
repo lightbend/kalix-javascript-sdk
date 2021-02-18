5eda0a7340dc1b6fd25a64aec0d779327b572a27
Peter Vlugter <pvlugter@users.noreply.github.com>
2021-02-18 17:11:58 +1300

    Publish protocols as jars

--- bin/prepare.sh
+++ bin/prepare.sh
@@ -5,7 +5,7 @@
 # Delete and recreate the proto directory
 rm -rf ./proto
 mkdir -p ./proto
-cp -r ../protocols/frontend/* ../protocols/protocol/* ./proto/
+cp -r ../protocols/proxy/src/main/protobuf/* ../protocols/sdk/src/main/protobuf/* ./proto/
 
 # Generate the protobuf bundle and typescript definitions
 pbjs -t static-module -w commonjs -o ./proto/protobuf-bundle.js -p ./proto -p ./protoc/include ./proto/akkaserverless/*.proto
