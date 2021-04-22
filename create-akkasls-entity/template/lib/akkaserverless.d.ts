3c0b7168b00aaeb55ee1a21842168bfeb6a88bae
Rory Stokes <contact@rorystok.es>
2021-04-22 18:19:52 +0930

    Update codegen to latest, and add value entities to type defs

--- create-akkasls-entity/template/lib/akkaserverless.d.ts
+++ create-akkasls-entity/template/lib/akkaserverless.d.ts
@@ -1,4 +1,5 @@
 import EventSourcedEntity from "@lightbend/akkaserverless-javascript-sdk/src/event-sourced-entity";
+import ValueEntity from "@lightbend/akkaserverless-javascript-sdk/src/value-entity";
 import Metadata from "@lightbend/akkaserverless-javascript-sdk/src/metadata";
 
 /**
@@ -8,7 +9,9 @@ import Metadata from "@lightbend/akkaserverless-javascript-sdk/src/metadata";
  */
 interface TypedEventSourcedEntity<State, EventHandlers, CommandHandlers>
   extends EventSourcedEntity {
-  setInitial: (callback: (entityId: string) => State) => void;
+  setInitial: (
+    callback: (entityId: string) => State
+  ) => TypedEventSourcedEntity<State, EventHandlers, CommandHandlers>;
 
   setBehavior: (
     callback: (
@@ -17,20 +20,51 @@ interface TypedEventSourcedEntity<State, EventHandlers, CommandHandlers>
       commandHandlers: CommandHandlers;
       eventHandlers: EventHandlers;
     }
-  ) => void;
+  ) => TypedEventSourcedEntity<State, EventHandlers, CommandHandlers>;
 }
 
 /**
- * The context passed to command handlers, enforcing the derived Event types
+ * A typed extension of the ValueEntity type enforcing State and Command types
+ * as generated from your proto. A TypeScript aware editor such as VS Code should leverage this
+ * to ensure all required handlers are implemented and provide hinting around types.
  */
-type EventSourcedCommandContext<Event> = {
+interface TypedValueEntity<State, CommandHandlers> extends ValueEntity {
+  setInitial: (
+    callback: (entityId: string) => State
+  ) => TypedValueEntity<State, CommandHandlers>;
+
+  setCommandHandlers: (
+    commandHandlers: CommandHandlers
+  ) => TypedValueEntity<State, CommandHandlers>;
+}
+
+/**
+ * The context passed to all command handlers.
+ */
+type CommandContext = {
   effect(
     method: unknown,
     message: unknown,
     synchronous: boolean,
     metadata: Metadata
   ): void;
-  emit(event: Event): void;
   fail(message: String): never;
   forward(method: unknown, message: string, metadata: Metadata);
+  entityId: string;
+  commandId: Long;
+};
+
+/**
+ * The context passed to EventSourced command handlers, enforcing the derived Event types
+ */
+type EventSourcedCommandContext<Event> = CommandContext & {
+  emit(event: Event): void;
+};
+
+/**
+ * The context passed to ValueEntity command handlers, enforcing the derived State type
+ */
+type ValueEntityCommandContext<State> = CommandContext & {
+  updateState(state: State): void;
+  deleteState(): void;
 };
