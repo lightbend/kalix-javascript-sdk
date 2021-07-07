c8d472b27c184419679d4dbf5c2e9ca182944792
Kikia Carter <kiki@lightbend.com>
2021-07-07 16:30:42 -0400

    feat: Update to support M1 architecture #99 (cla check)

--- sdk/bin/download-protoc.js
+++ sdk/bin/download-protoc.js
@@ -42,7 +42,7 @@ function determineDownloadFile() {
         case 'x64':
           return makeDownloadFile('osx-x86_64');
         case "arm64":
-          return makeDownloadFile("osx-x86_64"); // to support M1 https://github.com/protocolbuffers/protobuf/pull/8557
+          return makeDownloadFile("osx-x86_64"); // ref https://github.com/protocolbuffers/protobuf/pull/8557
       }
       break;
   }
