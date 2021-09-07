54e32cfd026dc4996d000f2d9ce6d7e4426506e9
János Szathmáry <29057504+janory@users.noreply.github.com>
2021-09-07 11:04:40 +0200

    feat: Add the TS version of the value entity example (#166)
    * Add the TS version of the value entity example
    
    * improve types
    
    * Update samples/ts-valueentity-shopping-cart/README.md
    
    Co-authored-by: Peter Vlugter <pvlugter@users.noreply.github.com>
    
    * fix typo
    
    * switch to the reply based API
    
    * switch to the reply based API
    
    Co-authored-by: Peter Vlugter <pvlugter@users.noreply.github.com>

---
+++ samples/ts-valueentity-shopping-cart/test/shoppingcart.test.ts
@@ -0,0 +1,113 @@
+/*
+ * Copyright 2021 Lightbend Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { MockValueEntity } from "./testkit";
+import { expect } from "chai";
+import shoppingcart from "../src/shoppingcart";
+import * as proto from "../lib/generated/proto";
+
+type State = proto.com.example.shoppingcart.domain.Cart;
+
+describe("ShoppingCartService", () => {
+  const entityId = "entityId";
+
+  describe("AddItem", () => {
+    it("should respond to addItem commands", () => {
+      const entity = new MockValueEntity<State>(shoppingcart, entityId);
+
+      entity.handleCommand(
+        "AddItem", { cartId: "cart1", productId: "a", name: "Apple", quantity: 1 });
+      entity.handleCommand(
+        "AddItem", { cartId: "cart1", productId: "b", name: "Banana", quantity: 2 });
+      entity.handleCommand(
+        "AddItem", { cartId: "cart1", productId: "c", name: "Cantaloupe", quantity: 3 });
+
+      expect(entity.error).to.be.undefined;
+      expect(entity.state.items)
+        .to.deep.equal(
+          [
+            { cartId: 'cart1', productId: 'a', name: 'Apple', quantity: 1 },
+            { cartId: 'cart1', productId: 'b', name: 'Banana', quantity: 2 },
+            { cartId: 'cart1', productId: 'c', name: 'Cantaloupe', quantity: 3 }
+          ]);
+    });
+  });
+
+  describe("RemoveItem", () => {
+    it("should remove items from a cart", () => {
+      const entity = new MockValueEntity<State>(shoppingcart, entityId);
+
+      entity.handleCommand(
+        "AddItem", { cartId: "cart1", productId: "a", name: "Apple", quantity: 1 });
+      entity.handleCommand(
+        "AddItem", { cartId: "cart1", productId: "b", name: "Banana", quantity: 2 });
+
+      expect(entity.error).to.be.undefined;
+      expect(entity.state.items)
+        .to.deep.equal(
+          [
+            { cartId: 'cart1', productId: 'a', name: 'Apple', quantity: 1 },
+            { cartId: 'cart1', productId: 'b', name: 'Banana', quantity: 2 }
+          ]);
+
+      entity.handleCommand(
+        "RemoveItem", { cartId: "cart1", productId: "a" });
+      expect(entity.error).to.be.undefined;
+      expect(entity.state.items)
+        .to.deep.equal(
+          [
+            { cartId: 'cart1', productId: 'b', name: 'Banana', quantity: 2 }
+          ]);
+    });
+  });
+
+  describe("GetCart", () => {
+    it("should default to an empty cart", async () => {
+      const entity = new MockValueEntity<State>(shoppingcart, entityId);
+      const result = entity.handleCommand("GetCart", { entityId });
+      expect(result).to.deep.equal({});
+      expect(entity.error).to.be.undefined;
+      expect(entity.state.items).to.be.empty;
+    });
+  });
+
+  describe("RemoveCart", () => {
+    it("should remove a cart", () => {
+
+      const entity = new MockValueEntity<State>(shoppingcart, entityId);
+
+      entity.handleCommand(
+        "AddItem", { cartId: "cart1", productId: "a", name: "Apple", quantity: 1 });
+      entity.handleCommand(
+        "AddItem", { cartId: "cart1", productId: "b", name: "Banana", quantity: 2 });
+      entity.handleCommand(
+        "AddItem", { cartId: "cart1", productId: "c", name: "Cantaloupe", quantity: 3 });
+
+      expect(entity.error).to.be.undefined;
+      expect(entity.state.items)
+        .to.deep.equal(
+          [
+            { cartId: 'cart1', productId: 'a', name: 'Apple', quantity: 1 },
+            { cartId: 'cart1', productId: 'b', name: 'Banana', quantity: 2 },
+            { cartId: 'cart1', productId: 'c', name: 'Cantaloupe', quantity: 3 }
+          ]);
+
+      entity.handleCommand(
+        "RemoveCart", { cartId: "cart1" })
+      expect(entity.state.items).to.be.empty
+    });
+  });
+});
