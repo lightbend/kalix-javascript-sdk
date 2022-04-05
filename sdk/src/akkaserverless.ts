2c4e7253ac05e057fcb845779703d1ce0f2157ae
Johan Andrén <johan@markatta.com>
2022-04-05 14:20:04 +0200

    fix: Status code was not accessible from typescript #284

--- sdk/src/akkaserverless.ts
+++ sdk/src/akkaserverless.ts
@@ -571,3 +571,26 @@ export class AkkaServerless {
     }
   }
 }
+
+/**
+ * The GRPC status codes.
+ */
+export enum GrpcStatus {
+  Ok = 0,
+  Cancelled = 1,
+  Unknown = 2,
+  InvalidArgument = 3,
+  DeadlineExceeded = 4,
+  NotFound = 5,
+  AlreadyExists = 6,
+  PermissionDenied = 7,
+  ResourceExhausted = 8,
+  FailedPrecondition = 9,
+  Aborted = 10,
+  OutOfRange = 11,
+  Unimplemented = 12,
+  Internal = 13,
+  Unavailable = 14,
+  DataLoss = 15,
+  Unauthenticated = 16,
+}
