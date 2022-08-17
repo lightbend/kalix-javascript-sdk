d6681dc4a0f588ad8c819bc0b3a5ea0ece2700f0
francisco <franciscolopezsancho@gmail.com>
2022-08-17 10:25:51 +0200

    format

--- sdk/src/kalix.ts
+++ sdk/src/kalix.ts
@@ -717,7 +717,8 @@ export class Kalix {
         proxyHostname:
           proxyInfo.internalProxyHostname ?? proxyInfo.proxyHostname,
         proxyPort:
-           proxyInfo.proxyPort ?? this.proxyPort ??
+          proxyInfo.proxyPort ??
+          this.proxyPort ??
           (proxyInfo.proxyHostname == 'localhost' ? 9000 : 80),
         identificationInfo: proxyInfo.identificationInfo || undefined,
       };
